9b671a64187a266644702b5a0d28434474274246
Jetsonhacks <jim@jetsonhacks.com>
2016-05-01 19:49:51 -0700

    Split installation of Qt Creator

--- installLibrealsense.sh
+++ installLibrealsense.sh
@@ -1,21 +1,22 @@
+#!/bin/bash
 # Install the Intel Realsense library librealsense on a Jetson Development Kit
 # Copyright (c) 2016 Jetsonhacks 
 # MIT License
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install qt5-default qtcreator -y
 sudo apt-get install libusb-1.0-0-dev
-./scripts/install_glfw3.sh
-sudo apt-get install qtcreator -y
-sudo ./scripts/install_qt.sh
 # Install into home directory
 cd ~/
 git clone https://github.com/jetsonhacks/librealsense.git
 cd librealsense
+# Install glfw3
+./scripts/install_glfw3.sh
+# Checkout the Jetson branch
 git checkout jetson
+# Copy over the udev rules so that camera can be run from user space
 sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/
 sudo cp config/uvc.conf /etc/modprobe.d/
 sudo udevadm control --reload-rules && udevadm trigger
-# Make
+# Make and Install
 make BACKEND=LIBUVC
 sudo make install
 
---
+++ installQtCreator.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Install Qt Creator
+sudo apt-get install qdbus qmlscene qt5-default qt5-qmake \
+ qtbase5-dev-tools qtchooser qtdeclarative5-dev xbitmaps \
+ xterm libqt5svg5-dev qttools5-dev qtscript5-dev \
+ qtdeclarative5-folderlistmodel-plugin qtdeclarative5-controls-plugin -y
+
