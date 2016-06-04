be4a37387bed6e15a13d871c1fd285e74e949950
jetsonhacks <jim@jetsonhacks.com>
2016-06-04 09:08:59 -0700

    Fix typo in shell file

--- installQtCreator.sh
+++ installQtCreator.sh
@@ -5,5 +5,5 @@ sudo apt-get install build-essential qdbus qmlscene qt5-default qt5-qmake \
  qtbase5-dev-tools qtchooser qtdeclarative5-dev xbitmaps \
  xterm libqt5svg5-dev qttools5-dev qtscript5-dev \
  qtdeclarative5-folderlistmodel-plugin qtdeclarative5-controls-plugin \
- qt5-default qtcreator -y-y
+ qt5-default qtcreator -y
 
