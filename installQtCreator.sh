c584d2d16512846fbf9f01cfb519ae6a17045f59
jetsonhacks <jim@jetsonhacks.com>
2016-06-04 09:07:37 -0700

    Install Qt Creator

--- installQtCreator.sh
+++ installQtCreator.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# Install Qt Creator
-sudo apt-get install qdbus qmlscene qt5-default qt5-qmake \
+# Install Qt and Qt Creator
+
+sudo apt-get install build-essential qdbus qmlscene qt5-default qt5-qmake \
  qtbase5-dev-tools qtchooser qtdeclarative5-dev xbitmaps \
  xterm libqt5svg5-dev qttools5-dev qtscript5-dev \
- qtdeclarative5-folderlistmodel-plugin qtdeclarative5-controls-plugin -y
+ qtdeclarative5-folderlistmodel-plugin qtdeclarative5-controls-plugin \
+ qt5-default qtcreator -y-y
 
