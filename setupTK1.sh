e4bdf6b05ee7b19887c39bed8c6624b124143d62
jetsonhacks <jetsonhacks.com>
2016-06-17 12:34:11 -0700

    Add USB 3.0 enabler

--- setupTK1.sh
+++ setupTK1.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Configure a NVIDIA Jetson 
+# and setup USB 3.0 port to run USB; usb_port_owner_info=2 indicates USB 3.0
+sudo sed -i 's/usb_port_owner_info=0/usb_port_owner_info=2/' /boot/extlinux/extlinux.conf
 # Disable USB autosuspend
 sudo sed -i '$s/$/ usbcore.autosuspend=-1/'  /boot/extlinux/extlinux.conf
 # /bin/ required for echo to work correctly in /bin/sh file
