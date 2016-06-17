073943f11f0f8e288195dc9329346bd8eac51ee3
Jetsonhacks <jim@jetsonhacks.com>
2016-06-16 21:55:47 -0700

    Update installKernelSources.sh

--- UVCKernelPatches/scripts/installKernelSources.sh
+++ UVCKernelPatches/scripts/installKernelSources.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-add-repository universe
 apt-get update
-apt-get install libqt4-dev pkg-config
+apt-get install libqt4-dev pkg-config -y
 cd /usr/src
 wget http://developer.download.nvidia.com/embedded/L4T/r21_Release_v4.0/source/kernel_src.tbz2
 tar -xvf kernel_src.tbz2
